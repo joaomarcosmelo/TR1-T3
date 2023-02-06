b119eb9b5e1272d3d0bc72472ab176f4cd560904
João Marcos <joo.marcos3@gmail.com>
2023-02-06 15:31:05 -0300

    Add files via upload

---
+++ simuladorenlace.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <stdlib.h>
+#include "camadaenlaceprotocolo.cpp"
+using namespace std;
+
+int main (){
+	int size = 32;
+	int quadro[size];
+
+	AplicacaoTransmissora();
+}
