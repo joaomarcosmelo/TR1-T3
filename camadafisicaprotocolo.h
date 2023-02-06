2a73601027f5ac370c1f8f619527796a26e5e293
João Marcos <joo.marcos3@gmail.com>
2023-02-06 15:29:43 -0300

    Add files via upload

---
+++ camadafisicaprotocolo.h
@@ -0,0 +1,30 @@
+#ifndef camadafisicaprotocolo.h
+#define camadafisicaprotocolo.h
+int CamadaFisicaReceptoraDecodificacaoBinaria();
+
+int CamadaDeAplicacaoReceptoraDecodificacaoManchester();
+
+int CamadaDeAplicacaoReceptoraDecodificacaoBipolar();
+
+void CamadaFisicaReceptora();
+
+void MeioDeComunicacao();
+
+void  CamadaFisicaTrasnmissoraCodificacaoBinaria();
+
+void CamadaDeAplicacaoTransmissoraCodificacaoManchester();
+
+void CamadaDeAplicacaoTransmissoraCodificacaoBipolar();
+
+void CamadaFisicaTransmissora(int quadro[]);
+
+void CamadaDeAplicacaoTransmissora();
+
+void AplicacaoTransmissora(void);
+
+void AplicacaoReceptora();
+
+void CamadaDeAplicacaoReceptora();
+
+
+#endif //camadafisicaprotocolo.h
